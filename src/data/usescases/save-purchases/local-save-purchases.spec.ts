4db9e76f89c9c0cc754836b2236cfbaaa4b17393
unknown <diegoferreira940@gmail.com>
2022-03-14 21:07:41 -0300

    ensure LocalSavePurchases throws if CacheStore.insert throws

--- src/data/usescases/save-purchases/local-save-purchases.spec.ts
+++ src/data/usescases/save-purchases/local-save-purchases.spec.ts
@@ -25,6 +25,12 @@ class CacheStoreSpy implements CacheStore {
 			throw new Error();
 		});
 	}
+
+	simulateInsertError(): void {
+		jest.spyOn(CacheStoreSpy.prototype, "insert").mockImplementationOnce(() => {
+			throw new Error();
+		});
+	}
 }
 
 const mockPurchases = (): Array<SavePurchases.Params> => [
@@ -85,5 +91,12 @@ describe("LocalSavePurchases", () => {
 		expect(cacheStore.insertCallsCount).toBe(1);
 		expect(cacheStore.insertKey).toBe("purchases");
 		expect(cacheStore.insertValues).toEqual(purchases);
+	});
+
+	test("should throw if insert throws", () => {
+		const { cacheStore, sut } = makeSut();
+		cacheStore.simulateInsertError();
+		const promise = sut.save(mockPurchases());
+		expect(promise).rejects.toThrow();
 	});
 });
